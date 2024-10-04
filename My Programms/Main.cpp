dea6a074b000b6a0f0a7c65f317257d826536214
RJSoftonist93 <crazyrobot1920@gmail.com>
2024-10-04 22:19:37 +0530

    miner changes

--- My Programms/Main.cpp
+++ My Programms/Main.cpp
@@ -5,5 +5,28 @@ int main()
 {
 	int UserChoice;
 
-	cout << "Hi Player "
+	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+	cout << "You can choose one of the following Pokémon:\n";
+	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+	cout << "Which Pokémon would you like to choose? Enter the number: ";
+	cin >> UserChoice;
+
+	if (UserChoice == 1)
+	{
+		cout << "You selected Bulbasaur. Great choice!";
+	}
+	else if (UserChoice == 2)
+	{
+		cout << "You selected Charmander. Nice Selection!!";
+	}
+	else if (UserChoice == 3)
+	{
+		cout << "You selected Squirtle. Awesome!!!";
+	}
+	else
+	{
+		cout << "Your choice is invalid. Please start the game again!";
+	}
+
+	return 0;
 }
