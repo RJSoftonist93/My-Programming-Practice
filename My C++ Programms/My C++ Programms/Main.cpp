0dc692e3a7fe346ed435410b89aea22551d8a95d
RJSoftonist93 <crazyrobot1920@gmail.com>
2024-10-08 08:40:32 +0530

    Pokemon selection script added

--- My C++ Programms/My C++ Programms/Main.cpp
+++ My C++ Programms/My C++ Programms/Main.cpp
@@ -1,32 +1,32 @@
-#include<iostream>
-using namespace std;
-
-int main()
-{
-	int UserChoice;
-
-	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
-	cout << "You can choose one of the following Pokémon:\n";
-	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
-	cout << "Which Pokémon would you like to choose? Enter the number: ";
-	cin >> UserChoice;
-
-	if (UserChoice == 1)
-	{
-		cout << "You selected Bulbasaur. Great choice!\n";
-	}
-	else if (UserChoice == 2)
-	{
-		cout << "You selected Charmander. Nice Selection!!\n";
-	}
-	else if (UserChoice == 3)
-	{
-		cout << "You selected Squirtle. Awesome!!!\n";
-	}
-	else
-	{
-		cout << "Your choice is invalid. Please start the game again!\n";
-	}
-
-	return 0;
-}
+//#include<iostream>
+//using namespace std;
+//
+//int main()
+//{
+//	int UserChoice;
+//
+//	cout << "Welcome to the world of Pokémon! I am Professor Oak.\n";
+//	cout << "You can choose one of the following Pokémon:\n";
+//	cout << "1. Bulbasaur\n2. Charmander\n3. Squirtle\n";
+//	cout << "Which Pokémon would you like to choose? Enter the number: ";
+//	cin >> UserChoice;
+//
+//	if (UserChoice == 1)
+//	{
+//		cout << "You selected Bulbasaur. Great choice!\n";
+//	}
+//	else if (UserChoice == 2)
+//	{
+//		cout << "You selected Charmander. Nice Selection!!\n";
+//	}
+//	else if (UserChoice == 3)
+//	{
+//		cout << "You selected Squirtle. Awesome!!!\n";
+//	}
+//	else
+//	{
+//		cout << "Your choice is invalid. Please start the game again!\n";
+//	}
+//
+//	return 0;
+//}
