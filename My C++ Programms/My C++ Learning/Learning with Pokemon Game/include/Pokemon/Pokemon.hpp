a6de463e228abe65e348dc22909dfb09afaf786d
Ishant Saxena <ishantsaxena@ymail.com>
2025-07-30 21:54:18 +0530

    Update Pokemon.hpp

--- My C++ Programms/My C++ Learning/Learning with Pokemon Game/include/Pokemon/Pokemon.hpp
+++ My C++ Programms/My C++ Learning/Learning with Pokemon Game/include/Pokemon/Pokemon.hpp
@@ -5,6 +5,7 @@
 #include "../../include/Pokemon/PokemonType.hpp"
 #include "../../include/Move/Move.hpp"
 using namespace std;
+using namespace N_Move;
 //#include "PokemonType.hpp" //instead of the full file we are using forward declaratiion
 //enum PokemonType; //forward declaration
 // 
@@ -13,67 +14,65 @@ using namespace std;
 namespace N_Pokemon
 { 
 
-class Pokemon {
-public: //everything below is public unless we add private: or protected:
-    struct Move;
-    enum class PokemonType;
-    vector <Move> moves; //store the list of moves here
+    class Pokemon {
+    public:
+        vector<Move> moves; // Vector to hold moves
 
-protected:   //everything below is protected unless we add private: or public:
-    string name;
-    PokemonType type;
-    int health;
-    int maxHealth;
-    int attackPower;
+    protected:   //everything below is protected unless we add private: or public:
+        string name;
+        PokemonType type;
+        int health;
+        int maxHealth;
+        int attackPower;
 
 
-public:  //everything below is public unless we add private: or protected:
-    //following are setter for accessing protected members of Pokemon class
-    string GetName()
-    {
-        return name;
-    }
-    PokemonType GetType()
-    {
-        return type;
-    }
-    int GetHealth()
-    {
-        return health;
-    }
-    int GetMaxHealth()
-    {
-        return maxHealth;
-    }
-    int GetAttackPower()
-    {
-        return attackPower;
-    }
+    public:  //everything below is public unless we add private: or protected:
+        //following are setter for accessing protected members of Pokemon class
+        string GetName()
+        {
+            return name;
+        }
+        PokemonType GetType()
+        {
+            return type;
+        }
+        int GetHealth()
+        {
+            return health;
+        }
+        int GetMaxHealth()
+        {
+            return maxHealth;
+        }
+        int GetAttackPower()
+        {
+            return attackPower;
+        }
 
-    // Default constructor
-    Pokemon();
+        // Default constructor
+        Pokemon();
 
-    // Parameterized constructor
-    Pokemon(string p_name, PokemonType p_type, int p_maxHealth, int p_attackPower);
+        // Parameterized constructor
+        Pokemon(string p_name, PokemonType p_type, int p_maxHealth, int p_attackPower);
 
-    // Copy constructor
-    Pokemon(const Pokemon& other);
+        // Copy constructor
+        Pokemon(const Pokemon& other);
 
-    void takeDamage(int damage);
-    bool isFainted() const;
+        void takeDamage(int damage);
+        bool isFainted() const;
 
-    // Destructor
-    ~Pokemon();
+        // Destructor
+        ~Pokemon();
 
-    // abstract function is used in Pokemon class so Pokemon class is Abstract class now. 
-    // no object of abstract class is createred. We can only create & use pointer object of abstract class
-    virtual void attack(Move selectedMove, Pokemon& target) = 0;
-    void Heal();
+        // abstract function is used in Pokemon class so Pokemon class is Abstract class now. 
+        // no object of abstract class is createred. We can only create & use pointer object of abstract class
+        virtual void attack(Move selectedMove, Pokemon& target) = 0;
+        void Heal();
 
-    int selectMove();
-    void useMove(Move selectedMove, Pokemon& target);
-    //base implementation for selecting and using move
-    void selectAndUseMove(Pokemon* target);
-    void printAvailableMoves();
-};
+        int selectMove();
+        void useMove(Move selectedMove, Pokemon& target);
+        //base implementation for selecting and using move
+        void selectAndUseMove(Pokemon* target);
+        void printAvailableMoves();
+    };
 }
